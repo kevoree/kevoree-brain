e0a47de10eb536f8e7aa5f9d7f467d414da43a6d
Francois Fouquet <fouquet.f@gmail.com>
2014-08-12 15:33:09 +0200

    fix

--- org.kevoree.brain.api/src/main/java/org/kevoree/brain/api/classifier/Classifier.java
+++ org.kevoree.brain.api/src/main/java/org/kevoree/brain/api/classifier/Classifier.java
@@ -7,8 +7,6 @@ public interface Classifier {
 
     public void setFeatureNames(String[] features);
 
-    public void setTrainingSize(int size);
-
     public void train(Object[] features, int supervisedClass);
 
     public void update();
