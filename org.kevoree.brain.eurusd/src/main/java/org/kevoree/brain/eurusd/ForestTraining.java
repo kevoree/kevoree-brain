1577944f66745b0bd844925b84d03088a6f32a5f
Assaad Moawad <assaad.moawad@uni.lu>
2015-02-06 18:14:30 +0100

    Adding random forests

---
+++ org.kevoree.brain.eurusd/src/main/java/org/kevoree/brain/eurusd/ForestTraining.java
@@ -0,0 +1,41 @@
+package org.kevoree.brain.eurusd;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.TreeMap;
+
+/**
+ * Created by assaad on 06/02/15.
+ */
+public class ForestTraining {
+
+    private ArrayList<LinearTraining> forest = new ArrayList<LinearTraining>();
+    private Random rand=new Random();
+
+    private TreeMap<Long, Double> eurUsd;
+
+    public void initialize(int forestSize,Long initTimeStamp, Long finalTimeStamp, int size, double alpha, int iteration, int space,TreeMap<Long, Double> eurUsd){
+       this.eurUsd=eurUsd;
+        for(int i=0;i<forestSize;i++){
+           // Long initTimeStamp, Long finalTimeStamp, int size, double alpha, int iteration, int numPoints, int space,TreeMap<Long, Double> eurUsd){
+
+            LinearTraining lt=new LinearTraining(initTimeStamp,finalTimeStamp,rand.nextInt(size)+1,alpha,iteration,rand.nextInt(space)+60000,eurUsd);
+            forest.add(lt);
+        }
+    }
+    public void train(int times){
+        for(LinearTraining lt: forest){
+            lt.train(times);
+        }
+    }
+
+    public double predict(long val){
+        double res=0;
+        for(LinearTraining lt: forest){
+            res+=lt.predict(val);
+        }
+        return res/forest.size();
+    }
+
+
+}
---
+++ org.kevoree.brain.eurusd/src/main/java/org/kevoree/brain/eurusd/TestTrain.java
@@ -0,0 +1,7 @@
+package org.kevoree.brain.eurusd;
+
+/**
+ * Created by assaad on 06/02/15.
+ */
+public class TestTrain {
+}
