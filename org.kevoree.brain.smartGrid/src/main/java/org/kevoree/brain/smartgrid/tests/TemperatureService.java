fd25b9dc4b1bd5fb9ea767dbd48cadd3690d0f37
Assaad Moawad <assaad.moawad@uni.lu>
2015-04-03 18:13:16 +0200

    Update temperature stats from airport just for fun :)

---
+++ org.kevoree.brain.smartGrid/src/main/java/org/kevoree/brain/smartgrid/tests/TemperatureService.java
@@ -0,0 +1,73 @@
+package org.kevoree.brain.smartgrid.tests;
+
+import org.kevoree.brain.smartgrid.util.TemperatureLoader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.TreeMap;
+
+/**
+ * Created by assaad on 03/04/15.
+ */
+public class TemperatureService {
+    public static void main(String[] args){
+        //String dir="/Users/assaad/work/github/data/Temperature/OLBA.txt";
+       // String dir="/Users/assaad/work/github/data/Temperature/ELLX.txt";
+        String dir="/Users/assaad/work/github/data/Temperature/LFMN.txt";
+        TreeMap<Long,Double> temperature = TemperatureLoader.load(dir);
+
+        double totalTemp=0;
+        double totalTempSq=0;
+        double min=100;
+        double max=-100;
+        int count=0;
+
+        int HoursInYear=365*24;
+
+        int mm=-10;
+        int mmax=40;
+        int interval=10;
+        int[] counters = new int[(mmax-mm)/interval+1];
+
+        for(Long l: temperature.keySet()){
+
+            Double var=temperature.get(l);
+            totalTemp+=var;
+            totalTempSq+=var*var;
+            count++;
+            if(var<min){
+                min=var;
+            }
+            if(var>max){
+                max=var;
+            }
+            int i= ((int)(var-mm))/interval;
+            counters[i]++;
+        }
+        double avg= totalTemp/count;
+        double variance = totalTempSq/count-avg*avg;
+
+       // System.out.println("total: "+count/2+" h or "+count/48+" days");
+        System.out.println("min: "+String.format( "%.2f",min)+", max: "+String.format( "%.2f",max)+" , avg: "+String.format( "%.2f",avg) + " , std: "+String.format("%.2f", Math.sqrt(variance)) );
+
+        int i=0;
+        for(int x=mm;x<mmax;x+=interval){
+            System.out.println("["+x+" , "+(x+interval)+"[: "+counters[i]*HoursInYear/count+" h or "+String.format( "%.2f",(((double)counters[i])*HoursInYear/count)/24)+" days, "+String.format( "%.2f",(((double)counters[i])*100/count))+" %");
+            i++;
+        }
+
+
+      /*  i=0;
+        for(int x=mm;x<mmax;x+=interval){
+            System.out.println("["+x+" , "+(x+interval)+"[: "+counters[i]*HoursInYear/count+" , "+String.format( "%.2f",(((double)counters[i])*HoursInYear/count)/24)+" , "+String.format( "%.2f",(((double)counters[i])*100/count)));
+            i++;
+        }*/
+
+
+
+
+
+
+
+    }
+}
---
+++ org.kevoree.brain.smartGrid/src/main/java/org/kevoree/brain/smartgrid/util/TemperatureLoader.java
@@ -0,0 +1,57 @@
+package org.kevoree.brain.smartgrid.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.util.TreeMap;
+
+/**
+ * Created by assaad on 03/04/15.
+ */
+public class TemperatureLoader {
+    public static TreeMap<Long,Double> load(String filename){
+        TreeMap<Long,Double> result=new TreeMap<Long, Double>();
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        int duplicate=0;
+        int linenum=0;
+
+        try {
+
+
+            br = new BufferedReader(new FileReader(filename));
+            int count=0;
+            while ((line = br.readLine()) != null) {
+                linenum++;
+                if (count < 6) {
+                    count++;
+                    continue;
+                }
+                String[] data = line.split(cvsSplitBy);
+
+
+                //ELLX,2014-01-01 00:20,4.00
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+                java.util.Date parsedDate = dateFormat.parse(data[1]);
+                Timestamp ts = new java.sql.Timestamp(parsedDate.getTime());
+                if(result.keySet().contains(ts.getTime())){
+                    duplicate++;
+                //    System.out.println("Duplicate ["+linenum+"]"+data[1]);
+                }
+                result.put(ts.getTime(),Double.parseDouble(data[2]));
+            }
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        System.out.println("Loaded " + result.size() + " values, duplicated: "+duplicate);
+        return result;
+    }
+}
