4f6e8ccb51f71ff9deb3643c8ca0a3c699048062
Assaad MOAWAD <assaad.moawad@uni.lu>
2016-10-06 12:02:56 +0200

    Update

--- org.kevoree.brain.oven/src/main/java/org/kevoree/brain/oven/experiment/RunningExample.java
+++ org.kevoree.brain.oven/src/main/java/org/kevoree/brain/oven/experiment/RunningExample.java
@@ -23,38 +23,53 @@ public class RunningExample {
         model.generateCsv("/Users/assaad/work/github/data/paulwurt.csv");
         Matrix m = model.generateMatrix();
 
-        Matrix backup=m.clone();
+        Matrix backup = m.clone();
 
-        exportCov(m, model, "/Users/assaad/work/github/data/paulwurtCov.csv",false);
+        exportCov(m, model, "/Users/assaad/work/github/data/paulwurtCov.csv", false);
 
         long starttime = System.currentTimeMillis();
         PCA x = new PCA(m, PCA.NORMALIZE);
         long endtime = System.currentTimeMillis();
         double d = endtime - starttime;
         System.out.println("Analysis took " + d + " ms for a matrix of size: " + m.rows() + "x" + m.columns());
-        exportCov(m, model, "/Users/assaad/work/github/data/paulwurtCovNorm.csv",true);
+        exportCov(m, model, "/Users/assaad/work/github/data/paulwurtCovNorm.csv", true);
 
-        Matrix mrand= new Matrix(null, m.rows(),m.columns());
-        Random rand=new Random();
-        for(int i=0;i<mrand.rows();i++){
-            for(int j=0;j<mrand.columns();j++){
-                mrand.set(i,j,rand.nextGaussian());
+        Matrix mrand = new Matrix(null, m.rows(), m.columns());
+        Random rand = new Random();
+        for (int i = 0; i < mrand.rows(); i++) {
+            for (int j = 0; j < mrand.columns(); j++) {
+                mrand.set(i, j, rand.nextGaussian());
             }
         }
 
         System.out.println();
         System.out.println("Generating pure random");
-        exportCov(mrand,model,"/Users/assaad/work/github/data/random.csv",false);
-        PCA y =new PCA(mrand, PCA.NORMALIZE);
-        exportCov(mrand,model,"/Users/assaad/work/github/data/randomNorm.csv",false);
-
-        Matrix temp=backup.clone();
-        x.setDimension(10);
-        temp=x.convertSpace(temp);
-
-        exportCov(temp,model,"/Users/assaad/work/github/data/convertedSpace.csv",true);
+        exportCov(mrand, model, "/Users/assaad/work/github/data/random.csv", false);
+        PCA y = new PCA(mrand, PCA.NORMALIZE);
+        exportCov(mrand, model, "/Users/assaad/work/github/data/randomNorm.csv", false);
 
 
+        for (int dim = 1; dim < backup.columns(); dim++) {
+            Matrix temp = backup.clone();
+            x.setDimension(dim);
+            temp = x.convertSpace(temp);
+            Matrix itemp = x.inverseConvertSpace(temp);
+
+            double err = 0;
+            double total = 0;
+            for (int i = 0; i < backup.rows(); i++) {
+                for (int j = 0; j < backup.columns(); j++) {
+                    err += (backup.get(i, j) - itemp.get(i, j)) * (backup.get(i, j) - itemp.get(i, j));
+                    total += backup.get(i, j) * backup.get(i, j);
+                }
+            }
+            total=Math.sqrt(total);
+            err = Math.sqrt(err);
+            System.out.println("features dim: "+dim+", error: " +err*100/total+"%");
+        }
+
+
+        // exportCov(temp,model,"/Users/assaad/work/github/data/convertedSpace.csv",true);
 
 
     }
@@ -71,12 +86,12 @@ public class RunningExample {
 
         double[][] covariance = overallProfile.getCovariance(overallProfile.getAvg());
 
-        int[] perm=new int[covariance.length];
-        for(int i=0;i<perm.length;i++){
-            perm[i]=i;
+        int[] perm = new int[covariance.length];
+        for (int i = 0; i < perm.length; i++) {
+            perm[i] = i;
         }
 
-        if(abs) {
+        if (abs) {
             for (int i = 0; i < covariance.length; i++) {
                 for (int j = 0; j < covariance.length; j++) {
                     covariance[i][j] = Math.abs(covariance[i][j]);
@@ -85,28 +100,28 @@ public class RunningExample {
             }
         }
 
-        double tempd=0;
-        int tempi=0;
+        double tempd = 0;
+        int tempi = 0;
 
         //sort:
-        for(int i=0;i<covariance.length;i++){
-            for(int j=i+1;j<covariance.length;j++){
-                if(covariance[j][j]>covariance[i][i]){
+        for (int i = 0; i < covariance.length; i++) {
+            for (int j = i + 1; j < covariance.length; j++) {
+                if (covariance[j][j] > covariance[i][i]) {
 
-                    tempi=perm[i];
-                    perm[i]=perm[j];
-                    perm[j]=tempi;
+                    tempi = perm[i];
+                    perm[i] = perm[j];
+                    perm[j] = tempi;
 
-                    for(int k=0;k<covariance.length;k++){
-                        tempd=covariance[i][k];
-                        covariance[i][k]=covariance[j][k];
-                        covariance[j][k]=tempd;
+                    for (int k = 0; k < covariance.length; k++) {
+                        tempd = covariance[i][k];
+                        covariance[i][k] = covariance[j][k];
+                        covariance[j][k] = tempd;
                     }
 
-                    for(int k=0;k<covariance.length;k++){
-                        tempd=covariance[k][i];
-                        covariance[k][i]=covariance[k][j];
-                        covariance[k][j]=tempd;
+                    for (int k = 0; k < covariance.length; k++) {
+                        tempd = covariance[k][i];
+                        covariance[k][i] = covariance[k][j];
+                        covariance[k][j] = tempd;
                     }
                 }
             }
