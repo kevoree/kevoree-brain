ff2c6db42972a571dbd4cc47b25dbf71dc304bfb
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-01-12 11:55:45 +0100

    lstm test

--- org.kevoree.brain.learning/src/main/java/com/evolvingstuff/reccurent/TestSequence.java
+++ org.kevoree.brain.learning/src/main/java/com/evolvingstuff/reccurent/TestSequence.java
@@ -1,7 +1,58 @@
 package com.evolvingstuff.reccurent;
 
+import com.evolvingstuff.reccurent.datasets.ModuloGeneration;
+import com.evolvingstuff.reccurent.datasets.TextGenerationUnbroken;
+import com.evolvingstuff.reccurent.datastructs.DataSet;
+import com.evolvingstuff.reccurent.model.Model;
+import com.evolvingstuff.reccurent.trainer.Trainer;
+import com.evolvingstuff.reccurent.util.NeuralNetworkHelper;
+
+import java.util.Random;
+
 /**
  * Created by assaad on 12/01/2017.
  */
 public class TestSequence {
+    public static void main(String[] arg) {
+
+        try {
+            Random rng = new Random(1234);
+            int training = 2000;
+            int testing = 100;
+            int inputsize = 13;
+            int outputsize = 7;
+            int timestep = 4;
+            int seqsize = 16;
+
+            String textSource = "modulo";
+            String savePath = "saved_models/"+textSource+".ser";
+            boolean initFromSaved = false; //set this to false to start with a fresh model
+            boolean overwriteSaved = true;
+
+
+            int bottleneckSize = inputsize; //one-hot input is squeezed through this
+            int hiddenDimension = (inputsize+outputsize)/2;
+            int hiddenLayers = 1;
+            double learningRate = 0.001;
+            double initParamsStdDev = 0.08;
+
+            ModuloGeneration data = new ModuloGeneration(inputsize, outputsize, timestep, seqsize, training, testing, rng);
+
+            Model lstm = NeuralNetworkHelper.makeLstmWithInputBottleneck(
+                    data.inputDimension, bottleneckSize,
+                    hiddenDimension, hiddenLayers,
+                    data.outputDimension, data.getModelOutputUnitToUse(),
+                    initParamsStdDev, rng);
+
+
+            int reportEveryNthEpoch = 50;
+            int trainingEpochs = 10000;
+
+            Trainer.train(trainingEpochs, learningRate, lstm, data, reportEveryNthEpoch, initFromSaved, overwriteSaved, savePath, rng);
+
+            System.out.println("done.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- org.kevoree.brain.learning/src/main/java/com/evolvingstuff/reccurent/datasets/ModuloGeneration.java
+++ org.kevoree.brain.learning/src/main/java/com/evolvingstuff/reccurent/datasets/ModuloGeneration.java
@@ -1,7 +1,123 @@
 package com.evolvingstuff.reccurent.datasets;
 
+import com.evolvingstuff.reccurent.autodiff.Graph;
+import com.evolvingstuff.reccurent.datastructs.DataSequence;
+import com.evolvingstuff.reccurent.datastructs.DataSet;
+import com.evolvingstuff.reccurent.datastructs.DataStep;
+import com.evolvingstuff.reccurent.loss.LossSoftmax;
+import com.evolvingstuff.reccurent.matrix.Matrix;
+import com.evolvingstuff.reccurent.model.LinearUnit;
+import com.evolvingstuff.reccurent.model.Model;
+import com.evolvingstuff.reccurent.model.Nonlinearity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by assaad on 12/01/2017.
  */
-public class ModuloGeneration {
+public class ModuloGeneration extends DataSet {
+    private int timesteps;
+    private int seqsize;
+    private Random rnd;
+
+    public ModuloGeneration(int inputVecSize, int outputVecSize, int timesteps, int seqsize, int training, int testing, Random rnd) {
+        this.inputDimension = inputVecSize;
+        this.outputDimension = outputVecSize;
+
+
+        this.timesteps = timesteps;
+        this.seqsize = seqsize;
+        this.rnd = rnd;
+        this.training = generate(training);
+        this.testing = generate(testing);
+        this.lossTraining = new LossSoftmax();
+        this.lossReporting = new LossSoftmax();
+
+    }
+
+
+    private List<DataSequence> generate(int size) {
+        List<DataSequence> list = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            ArrayList<DataStep> dataSteps = new ArrayList<>();
+            int[] seeds = new int[timesteps];
+            int totalseed = 0;
+            int modulo = 0;
+
+            for (int j = 0; j < seqsize; j++) {
+                modulo = j % timesteps;
+                totalseed -= seeds[modulo];
+                seeds[modulo] = rnd.nextInt(inputDimension);
+                totalseed += seeds[modulo];
+                if (j < timesteps) {
+                    dataSteps.add(new DataStep(getArray(seeds[modulo], inputDimension), null));
+                } else {
+                    dataSteps.add(new DataStep(getArray(seeds[modulo], inputDimension), getArray(totalseed, outputDimension)));
+                }
+            }
+            DataSequence dataSequence = new DataSequence(dataSteps);
+            list.add(dataSequence);
+        }
+        return list;
+    }
+
+    private double[] getArray(int seed, int dim) {
+        double[] arr = new double[dim];
+        arr[seed % dim] = 1;
+        return arr;
+    }
+
+
+
+    private int getPosition(double[] output){
+        double max=output[0];
+        int pos=0;
+        for(int i=1;i<output.length;i++){
+            if(max<output[i]){
+                max=output[i];
+                pos=i;
+            }
+        }
+        return pos;
+    }
+
+    @Override
+    public void DisplayReport(Model model, Random rng) throws Exception {
+        DataSequence seq = generate(1).get(0);
+        model.resetState();
+        Graph g = new Graph(false);
+        String seqreal="Real seq: ";
+        String seqpred="Pred seq: ";
+
+        int total=0;
+        int totalmatch=0;
+
+
+        for (DataStep step : seq.steps) {
+            Matrix output = model.forward(step.input, g);
+            if (step.targetOutput != null) {
+                int x=getPosition(step.targetOutput.w);
+                int y=getPosition(output.w);
+                seqreal+=x+", ";
+                seqpred+=y+", ";
+                if(x==y){
+                    totalmatch++;
+                }
+                total++;
+            }
+        }
+        System.out.println(seqreal);
+        System.out.println(seqpred);
+        System.out.println("matched: "+totalmatch+ " / "+total);
+
+
+    }
+
+    @Override
+    public Nonlinearity getModelOutputUnitToUse() {
+        return new LinearUnit();
+    }
 }
